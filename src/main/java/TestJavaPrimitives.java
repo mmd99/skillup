ff19c439b02be42a7bb7d1a77d3168734519385f
mmd99 <89353352+mmd99@users.noreply.github.com>
2021-08-22 22:18:43 +0300

    Delete src directory

--- src/main/java/TestJavaPrimitives.java
+++
@@ -1,9 +0,0 @@
-public class TestJavaPrimitives {
-    public static void main(String[] args) {
-        byte a = 2;
-        byte b = 5;
-        byte c =20;
-        int res = a+b+c;
-        System.out.println(res);
-    }
-}
